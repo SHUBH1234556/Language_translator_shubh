bea8c3f287c7a19c75f0e8fbe8467cba03e81d80
SHUBH1234556 <97507089+SHUBH1234556@users.noreply.github.com>
2023-09-23 13:50:47 +0530

    specifying number of languages used

---
+++ languages.js
@@ -0,0 +1,52 @@
+// Define an array of languages with their properties
+const languages = [
+  {
+    no: "0",
+    name: "Auto",
+    native: "Detect",
+    code: "auto",
+  },
+  {
+    no: "1",
+    name: "Afrikaans",
+    native: "Afrikaans",
+    code: "af",
+  },
+  {
+    no: "2",
+    name: "Albanian",
+    native: "Shqip",
+    code: "sq",
+  },
+  // ... (continue listing other languages)
+
+  // Example of a language entry:
+  {
+    no: "55",
+    name: "Urdu",
+    native: "اردو",
+    code: "ur",
+  },
+  {
+    no: "56",
+    name: "Vietnamese",
+    native: "Tiếng Việt",
+    code: "vi",
+  },
+  {
+    no: "57",
+    name: "Welsh",
+    native: "Cymraeg",
+    code: "cy",
+  },
+  {
+    no: "58",
+    name: "Yiddish",
+    native: "ייִדיש",
+    code: "yi",
+  },
+];
+
+// You can use this array of languages in your code as needed.
+// The logic of the code depends on how you plan to use it.
+// Feel free to modify other parts of your code as necessary.
